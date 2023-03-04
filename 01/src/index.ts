c28acff533d29b13e2a7d06a8b86bc4ea1dadabd
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-04 14:23:42 +0530

    funtions introduct

--- 01/src/index.ts
+++ 01/src/index.ts
@@ -14,3 +14,21 @@ let numbersArray=[10,20,30,40,50,"nipuna"]
 
 //tuples
 let person:[number,String]=[2,"piyumal"]
+
+
+//funtions
+function calculate(){
+    //1  
+    // return 2; //return type rka number auto dagannava
+    //2 
+    //return "nipuna"; //return type rka String auto dagannava
+
+    //3
+    let numbers:number[] =[]
+    return numbers;     //return type rka Array auto dagannava
+}
+
+function calculateAvarageMarks(subjectCount:number,totalMark:number):number{
+    let ava=(totalMark/subjectCount)*100
+    return ava;
+}
